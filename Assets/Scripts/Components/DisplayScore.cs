babe68d15f206ff53c0f20b4ae131ea5a5708a18
zoniventris <f.zoniventris@gmail.com>
2022-09-02 16:11:30 -0700

    Get score display format from initial text string.

--- Assets/Scripts/Components/DisplayScore.cs
+++ Assets/Scripts/Components/DisplayScore.cs
@@ -6,9 +6,25 @@ public class DisplayScore : MonoBehaviour
     public Text highScoreDisplay;
     public Text currentScoreDisplay;
 
+    private string highScoreFormat;
+    private string currentScoreFormat;
+
+    private void Start()
+    {
+        highScoreFormat = highScoreDisplay.text;
+        currentScoreFormat = currentScoreDisplay.text;
+    }
+
     private void Update()
     {
-        highScoreDisplay.text = $"High Score: {PlayerPrefs.GetInt("HighScore", 0)}";
-        currentScoreDisplay.text = $"Current Score: {PlayerPrefs.GetInt("CurrentScore", 0)}";
+        if (highScoreDisplay != null)
+        {
+            highScoreDisplay.text = string.Format(highScoreFormat, PlayerPrefs.GetInt("HighScore", 0));
+        }
+
+        if (currentScoreDisplay != null)
+        {
+            currentScoreDisplay.text = string.Format(currentScoreFormat, PlayerPrefs.GetInt("CurrentScore", 0));
+        }
     }
 }
