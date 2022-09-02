b78fd7d8fbd496b93065bc7b16ad541bc473c7ef
zoniventris <f.zoniventris@gmail.com>
2022-09-02 14:54:53 -0700

    Display explosion on fire spell destruction.

--- Assets/Scripts/Components/Projectile.cs
+++ Assets/Scripts/Components/Projectile.cs
@@ -8,9 +8,20 @@ public class Projectile : MonoBehaviour
     public Animator animator;
     public Rigidbody2D rb;
 
+    public GameObject destructionFX;
+
     private void Start()
     {
         animator.SetFloat("Direction", DirectionUtil.ToAnimationDirection(direction));
         rb.velocity = DirectionUtil.ToVector(direction) * speed;
+    }
+
+    private void OnDestroy()
+    {
+        if (destructionFX != null)
+        {
+            GameObject go = Instantiate(destructionFX);
+            go.transform.position = transform.position;
+        }
     }
 }
