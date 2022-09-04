b607cd81b47ed05b4725ea10458d21c052ab74c4
zoniventris <f.zoniventris@gmail.com>
2022-09-03 17:30:47 -0700

    Remove extra SpawnController component & tweak some values.

--- Assets/Scripts/Components/SpawnController.cs
+++ Assets/Scripts/Components/SpawnController.cs
@@ -25,7 +25,7 @@ public class SpawnController : MonoBehaviour
         timeInWave = 0.0f;
         foreach (var rule in spawnRules)
         {
-            float estimatedSpawnEnd = rule.time + rule.number * spawnCooldown;
+            float estimatedSpawnEnd = rule.time + rule.number * spawnCooldown + 5;
             if (estimatedSpawnEnd > maxWaveTime)
             {
                 maxWaveTime = estimatedSpawnEnd;
@@ -35,12 +35,6 @@ public class SpawnController : MonoBehaviour
 
     private void Update()
     {
-        // I don't know how or why this happens, but it apparently does when the game first starts...?
-        if (spawnRules.Count == 0)
-        {
-            return;
-        }
-
         foreach (SpawnRule rule in spawnRules)
         {
             if (rule.time >= timeInWave && rule.time < timeInWave + Time.deltaTime)
